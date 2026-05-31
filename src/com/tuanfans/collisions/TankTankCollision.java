2773e18077e69ec67def2665fa807152cc345e21
tuanfans-hub <2171343856@qq.com>
2026-05-31 20:42:17 +0800

    使用责任链设计模式，添加链式碰撞检测

---
+++ src/com/tuanfans/collisions/TankTankCollision.java
@@ -0,0 +1,23 @@
+package com.tuanfans.collisions;
+
+import com.tuanfans.AbstractGameObject;
+import com.tuanfans.tank.Tank;
+import com.tuanfans.wall.Wall;
+
+/**
+ * @author TuanFans
+ * @date 2026/5/31
+ */
+public class TankTankCollision implements Collision{
+    @Override
+    public boolean collide(AbstractGameObject ago1, AbstractGameObject ago2) {
+        if(ago1 instanceof Tank tank1 && ago2 instanceof Tank tank2){
+            if(!tank1.isLive()||!tank2.isLive()) return false;
+            if(tank1.getRect().intersects(tank2.getRect())){
+                tank1.back();
+                tank2.back();
+            }
+        }
+        return true;
+    }
+}
